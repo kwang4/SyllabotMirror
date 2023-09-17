1f911e1fddf267ebb327dea4fb61eff0e158b21c
Daniel Buchanan <dsbuchan@ncsu.edu>
2023-09-17 19:49:19 -0400

    Debugging Dockerfile so that it works for backend

--- backend/src/APIRoutes.js
+++ backend/src/APIRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const apiRouter = express.Router();
 
 apiRouter.use(express.json());
-apiRouter.use()
 
 
 module.exports = apiRouter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,16 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000;
 
 const apiRouter = require('./APIRoutes');
 app.use(apiRouter);
 
 app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
       
 
 // As our server to listen for incoming connections
