7f8310bc43961b78df68a4b4eb54a4ba3cf8c350
cmriggs <cmriggs@ncsu.edu>
2023-10-21 10:07:07 -0400

    Updating to the correct Slack index.js

--- backend/Slack/index.js
+++ backend/Slack/index.js
@@ -16,7 +16,22 @@ const app = new App({
   console.log('App is running');
 })();
 
-// Response to messages that contain hello
-app.message('hello', async ({message, say}) => {
-  await say(`Hello <@${message.user}>!`);
-})
+app.command('/ask', async({command, ack, client}) => {
+
+  // Need to add try/catch error handling scenarios, but this is good skeleton
+  // Reworked from using built in say/respond functions to using client
+
+  // The parameter is what prevents user message from being deleted
+  await ack({response_type: 'in_channel'});
+
+  // TODO Make API call to get response
+
+  //console.log(JSON.stringify(command))
+  var responseMessage = `Q: \"${command.text}\" asked by <@${command.user_name}>\nA: This is the answer to your question!`
+
+  // Send message back 
+  await client.chat.postMessage({
+    channel: command.channel_id,
+    text: responseMessage
+  });
+});
