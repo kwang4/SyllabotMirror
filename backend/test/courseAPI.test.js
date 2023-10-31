789becaa83520d632cae7a680adb1317e9051ae9
Brandon Partin <blpartin@ncsu.edu>
2023-10-31 12:07:25 -0400

    Tested Creating Test Cases
    Added a simple check for JSON objects that we will use once Dr. Dominguez gets back to us on how to run the tests with Docker.

--- backend/test/courseAPI.test.js
+++ backend/test/courseAPI.test.js
@@ -1,6 +1,12 @@
 //import courseAPI;
 const CourseDAO = require('../src/data/CourseDAO');
 
+var course1 = {
+  "courseID": 1,
+  "semesterID": 1,
+  "courseName": "Senior Design"
+}
+
 test('adds 1 + 2 to equal 3', () => {
   console.log(CourseDAO.getCourse(1, 1));
   expect(1+2).toBe(3);
@@ -9,3 +15,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 3 - 2 to equal 1', () => {
   expect(3-2).toBe(1);
 });
+
+// test('course1 looks like this', () => {
+//   expect(CourseDAO.getCourseByID(1)).toBe(course1);
+// });
