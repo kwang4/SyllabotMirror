7289b4cab0b630987bfb01de61a8e7fd5a33459d
Daniel Buchanan <dsbuchan@ncsu.edu>
2023-11-04 18:04:52 -0400

    Correcting uploadFile to add to table

--- backend/src/data/ResourceDAO.js
+++ backend/src/data/ResourceDAO.js
@@ -18,9 +18,8 @@ function getCourseFiles(scr_sec_number, scr_crs_id) {
 function uploadFile(scr_sec_number, scr_crs_id, original_name, file_path, file_parsedpath){
   return db.query('INSERT INTO file (fil_link, fil_name, fil_parsed_link) VALUES (?, ?, ?)', [file_path, original_name, file_parsedpath]).then(()=>{
     return getFile(file_path, original_name).then(results =>{
-      let file = results.map(file => new File(file));
-      return db.query('INSERT INTO section_resource (scr_crs_id, scr_sec_number, scr_fil_id) VALUES (?,?,?)', [scr_sec_number, scr_crs_id, file.fil_id]).then(()=>{
-        return file;
+      return db.query('INSERT INTO section_resource (scr_crs_id, scr_sec_number, scr_fil_id) VALUES (?,?,?)', [scr_sec_number, scr_crs_id, results[0].id]).then(()=>{
+        return results;
       });
     });
   });
