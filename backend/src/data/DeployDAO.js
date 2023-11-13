3893017ee837f1c89207c706cdd2e6274dacd2dd
cmriggs <cmriggs@ncsu.edu>
2023-11-13 09:51:05 -0500

    Move API stuff for bot connections

--- backend/src/data/DeployDAO.js
+++ backend/src/data/DeployDAO.js
@@ -5,8 +5,16 @@ const Deploy = require('./models/Deploy')
 function getDeploys() {
   return db.query('SELECT * FROM deploy').then(({ results }) => {
     return results.map(deploy => new Deploy(deploy));
+  });
+}
+
+/*
+function getDeploysBySection(sec_crs_id, sec_num) {
+  return db.query('SELECT * FROM').then(( { results }) => {
+    return results.map(deploy => new Deploy(deploy))
   })
 }
+*/
 
 module.exports = {
   getDeploys: getDeploys
