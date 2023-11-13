1a6164185410712be6162c166f5efe73fc1297e8
jacksondhall <jacksondhall02@gmail.com>
2023-11-13 11:00:05 -0500

    Worked on rosterDAO tests
    Pair program with Brandon

--- backend/test/rosterDAO.test.js
+++ backend/test/rosterDAO.test.js
@@ -1,6 +1,16 @@
 const vars = require('../test/testVariables');
 const RosterDAO = require('../src/data/RosterDAO');
 const testVariables = require('../test/testVariables');
+const UserDAO = require('../src/data/UserDAO');
+
+function addUserToRosterHelper(crs_id, sec_num, user, role_id) {
+  RosterDAO.addUserToRoster(crs_id, sec_num, user, role_id).then(temp => {
+    expect(temp[0].courseID).toEqual(crs_id);
+    expect(temp[0].sectionID).toEqual(sec_num);
+    expect(temp[0].roleID).toEqual(role_id);
+    UserDAO.getUserByUnityID("test2id").then(tempUser=>{expect(temp[0].userID).toEqual(tempUser[0].unity_id)});
+  });
+}
 
 //PASSING
 describe('TEST getRosters', () => {
@@ -39,18 +49,15 @@ describe('TEST getRoster', () => {
 //   }, 1000);
 // });
 
-//FAILING (SO CLOSE TO BEING DONE. It's something in this last step which I'm checking with the console.log on line 50)
+//PASSING
 describe('TEST addUserToRoster', () => {
   it('Generic Test', done => {
     var crs_id = 5;
     var sec_num = 100;
     var user = testVariables.testUser2[0]; //THIS NEEDS TO BE A USER OBJECT, NOT JUST THE ID
     var role_id = 1;
-    RosterDAO.addUserToRoster(crs_id, sec_num, user, role_id).then(temp => {
-      console.log(temp);
-      expect(temp.id).toEqual(user.unity_id);
-      done();
-    });
+    addUserToRosterHelper(crs_id, sec_num, user, role_id);
+    done();
   }, 1000);
 });
 
@@ -69,27 +76,31 @@ describe('TEST updateUserRole', () => {
   }, 1000);
 });
 
-// //PASSING (May Want to Change Criteria so Only Test Data is Deleted and then Change .toEqual(?))
-// describe('TEST deleteEntireRoster', () => {
-//   it('Generic Test', done => {
-//     var crs_id = 5;
-//     var sec_num = 100;
-//     RosterDAO.deleteEntireRoster(crs_id, sec_num).then(temp => {
-//       expect(temp).toEqual(2);
-//       done();
-//     });
-//   }, 1000);
-// });
+//PASSING (May Want to Change Criteria so Only Test Data is Deleted and then Change .toEqual(?))
+describe('TEST deleteEntireRoster', () => {
+  it('Generic Test', done => {
+    var crs_id = 5;
+    var sec_num = 100;
+    RosterDAO.deleteEntireRoster(crs_id, sec_num).then(temp => {
+      expect(temp).toEqual(1);
+      done();
+    });
+  }, 1000);
+});
 
-// // I believe this is failing due to usr_id not being an Int
-// describe('TEST deleteUserFromRoster', () => {
-//   it('Generic Test', done => {
-//     var crs_id = 5;
-//     var sec_num = 100;
-//     var usr_id = "test2id";
-//     RosterDAO.deleteUserFromRoster(crs_id, sec_num, usr_id).then(temp => {
-//       expect(temp).toEqual(1);
-//       done();
-//     });
-//   }, 1000);
-// });
+// FAILING
+describe('TEST deleteUserFromRoster', () => {
+  it('Generic Test', done => {
+    var crs_id = 5;
+    var sec_num = 100;
+    var user = testVariables.testUser2[0];
+    var role_id = 1;
+    addUserToRosterHelper(crs_id, sec_num, user, role_id);
+    UserDAO.getUserByUnityID("test2id").then(tempUser=>{
+      RosterDAO.deleteUserFromRoster(crs_id, sec_num, tempUser.usr_id).then(temp => {
+        expect(temp).toEqual(1);
+        done();
+      });
+    });
+  }, 1000);
+});
