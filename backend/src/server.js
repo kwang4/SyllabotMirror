d118b98652f3b7bf5c319222c8be417af08f5b04
cmriggs <cmriggs@ncsu.edu>
2023-11-19 17:16:22 -0500

    Getting deploys on startup
    Co-Authored-By: dsbuchan <19684+dsbuchan@users.noreply.github.ncsu.edu>
    Co-Authored-By: jdhall9 <20253+jdhall9@users.noreply.github.ncsu.edu>

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ PORT = process.env.PORT;
 app.use(express.json());
 app.use(apirouter)
 
+const DeployDAO = require('./data/DeployDAO.js');
+
 
 app.use(express.urlencoded({extended: true}));
 
@@ -17,6 +19,11 @@ app.get('/', (req, res) => {
 if(PORT == null)
   PORT = 80;
 // As our server to listen for incoming connections
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT, async () => {
+  console.log(`Server listening on port: ${PORT}`);
+
+  // Get all deploys
+  let deploys = await DeployDAO.getDeploys();
+});
 
 module.exports = {app:app}
